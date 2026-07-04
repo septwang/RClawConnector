6949eb36f072b7ea8510098e0ef1b1321c75f462
rclaw <wr_1988@me.com>
2026-07-04 20:34:55 +0800

    Add files via upload

---
+++ download-macos.sh
@@ -0,0 +1,17 @@
+{\rtf1\ansi\ansicpg936\cocoartf2870
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;\f1\fnil\fcharset134 PingFangSC-Regular;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 #!/bin/bash\
+echo "
+\f1 \'d5\'fd\'d4\'da\'b4\'d3\'b9\'d9\'cd\'f8\'cf\'c2\'d4\'d8
+\f0  RClaw Connector..."\
+curl -L -o RClawConnector_macOS.dmg "{\field{\*\fldinst{HYPERLINK "https://www.rclaw.me/assets/download/frontend/macos/RClawConnector_1.0.0_aarch64.dmg"}}{\fldrslt https://www.rclaw.me/assets/download/frontend/macos/RClawConnector_1.0.0_aarch64.dmg}}"\
+echo "
+\f1 \'cf\'c2\'d4\'d8\'cd\'ea\'b3\'c9\'a3\'a1\'c7\'eb\'b4\'f2\'bf\'aa
+\f0 DMG
+\f1 \'b0\'b2\'d7\'b0\'a1\'a3
+\f0 "}
