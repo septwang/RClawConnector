5018280dda8a38dec63d95f75cb8444c31a04c65
rclaw <wr_1988@me.com>
2026-07-04 20:44:58 +0800

    Simplify download script for RClaw Connector

--- download-win.bat
+++ download-win.bat
@@ -1,16 +1,5 @@
-{\rtf1\ansi\ansicpg936\cocoartf2870
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;\f1\fnil\fcharset134 PingFangSC-Regular;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
-
-\f0\fs24 \cf0 @echo off\
-echo 
-\f1 \'d5\'fd\'d4\'da\'b4\'d3\'b9\'d9\'cd\'f8\'cf\'c2\'d4\'d8
-\f0  RClaw Connector...\
-curl -L -o RClawConnector_Windows.exe "{\field{\*\fldinst{HYPERLINK "https://www.rclaw.me/assets/download/frontend/windows/RClawConnector_1.0.0_x64_zh-CN.msi"}}{\fldrslt https://www.rclaw.me/assets/download/frontend/windows/RClawConnector_1.0.0_x64_zh-CN.msi}}"\
-echo 
-\f1 \'cf\'c2\'d4\'d8\'cd\'ea\'b3\'c9\'a3\'a1\'c7\'eb\'d4\'cb\'d0\'d0\'b0\'b2\'d7\'b0\'b0\'fc\'a1\'a3
-\f0 \
-pause}
+@echo off
+echo Downloading RClaw Connector for Windows...
+curl -L -o RClawConnector_Windows.exe "https://www.rclaw.me/downloads/RClawConnector_Windows.exe"
+echo Download complete! Run the installer to proceed.
+pause
